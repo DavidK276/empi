d8c5c0a21464aad7809ec154c99d1b6a855b89e0
David Krchňavý <david.krchnavy@gmail.com>
2024-04-11 10:47:51 +0200

    fix text entry type

--- empi-web/src/lib/constants.ts
+++ empi-web/src/lib/constants.ts
@@ -3,3 +3,4 @@ export const API_ENDPOINT = DJANGO_SERVER_URL + 'api/';
 export const TOKEN_COOKIE = 'sessionid';
 export const SELF_URL_COOKIE = 'userurl';
 export const SUPPORTED_LANGS = ['en', 'sk'];
+export const ALLOW_TEXTENTRY_ATTR = false;
--- empi-web/src/routes/attributes/Attribute.svelte
+++ empi-web/src/routes/attributes/Attribute.svelte
@@ -1,10 +1,12 @@
 <script lang="ts">
 
 	import Option from './Option.svelte';
+
 	import type { Attribute } from '$lib/objects/attribute';
 	import { box } from '$lib/style.css';
 	import { t } from '$lib/translations';
 	import { vars } from '$lib/theme.css';
+	import { ALLOW_TEXTENTRY_ATTR } from '$lib/constants';
 
 	function addOption(e: Event) {
 		const target = e.target as HTMLButtonElement;
@@ -13,49 +15,56 @@
 	}
 
 	export let attr: Attribute | null = null;
+	let chosen_type: string;
 </script>
 {#if attr != null}
 	<div class="{box}">
-	<form method="POST">
-		<label for="name" style="margin: 0">{$t('attrs.name')}</label>
-		<input type="text" name="name" id="name" value="{attr.name}" required>
-		<label for="type">{$t('attrs.type')}</label>
-		<select name="type" id="type" disabled>
-			<option value="SC" selected="{attr.type === 'SC'}">{$t('attrs.single_choice')}</option>
-			<option value="MC" selected="{attr.type === 'MC'}">{$t('attrs.multiple_choice')}</option>
-			<option value="ET" selected="{attr.type === 'ET'}">{$t('attrs.enter_text')}</option>
-		</select>
-		{#if attr.type !== 'EC'}
-			<fieldset id="options">
-				<legend>{$t('attrs.options')}</legend>
-				{#each attr.values as value}
-					<Option {value}></Option>
-				{/each}
-				<button type="button" style="margin-top: {vars.sm}" on:click={addOption}>+</button>
-			</fieldset>
-		{/if}
-		<input type="hidden" name="url" value="{attr.url}">
-		<button type="submit">{$t('common.submit')}</button>
-		<button type="submit" name="delete" style="background-color: {vars.danger}">{$t('attrs.delete')}</button>
-	</form>
-</div>
+		<form method="POST">
+			<label for="name" style="margin: 0">{$t('attrs.name')}</label>
+			<input type="text" name="name" id="name" value="{attr.name}" required>
+			<label for="type">{$t('attrs.type')}</label>
+			<select name="type" id="type" disabled>
+				<option value="SC" selected="{attr.type === 'SC'}">{$t('attrs.single_choice')}</option>
+				<option value="MC" selected="{attr.type === 'MC'}">{$t('attrs.multiple_choice')}</option>
+				{#if ALLOW_TEXTENTRY_ATTR}
+					<option value="ET" selected="{attr.type === 'ET'}">{$t('attrs.enter_text')}</option>
+				{/if}
+			</select>
+			{#if attr.type !== 'ET'}
+				<fieldset id="options">
+					<legend>{$t('attrs.options')}</legend>
+					{#each attr.values as value}
+						<Option {value}></Option>
+					{/each}
+					<button type="button" style="margin-top: {vars.sm}" on:click={addOption}>+</button>
+				</fieldset>
+			{/if}
+			<input type="hidden" name="url" value="{attr.url}">
+			<button type="submit">{$t('common.submit')}</button>
+			<button type="submit" name="delete" style="background-color: {vars.danger}">{$t('attrs.delete')}</button>
+		</form>
+	</div>
 {:else}
 	<div class="{box}">
-	<form method="POST">
-		<label for="name" style="margin: 0">{$t('attrs.name')}</label>
-		<input type="text" name="name" id="name" required>
-		<label for="type">{$t('attrs.type')}</label>
-		<select name="type" id="type">
-			<option value="SC">{$t('attrs.single_choice')}</option>
-			<option value="MC">{$t('attrs.multiple_choice')}</option>
-			<option value="ET">{$t('attrs.enter_text')}</option>
-		</select>
-			<fieldset id="options">
-				<legend>{$t('attrs.options')}</legend>
-				<button type="button" style="margin-top: {vars.sm}" on:click={addOption}>+</button>
-			</fieldset>
-		<input type="hidden" name="create">
-		<button type="submit">{$t('common.submit')}</button>
-	</form>
-</div>
+		<form method="POST">
+			<label for="name" style="margin: 0">{$t('attrs.name')}</label>
+			<input type="text" name="name" id="name" required>
+			<label for="type">{$t('attrs.type')}</label>
+			<select name="type" id="type" bind:value={chosen_type}>
+				<option value="SC">{$t('attrs.single_choice')}</option>
+				<option value="MC">{$t('attrs.multiple_choice')}</option>
+				{#if ALLOW_TEXTENTRY_ATTR}
+					<option value="ET">{$t('attrs.enter_text')}</option>
+				{/if}
+			</select>
+			{#if chosen_type !== 'ET'}
+				<fieldset id="options">
+					<legend>{$t('attrs.options')}</legend>
+					<button type="button" style="margin-top: {vars.sm}" on:click={addOption}>+</button>
+				</fieldset>
+			{/if}
+			<input type="hidden" name="create">
+			<button type="submit">{$t('common.submit')}</button>
+		</form>
+	</div>
 {/if}
