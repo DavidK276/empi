0149974e91e38c81357abf2ba2f3b3fa99ef8a9a
David Krchňavý <david.krchnavy@gmail.com>
2024-04-11 10:47:29 +0200

    redirect to home if user is not staff

--- empi-web/src/routes/attributes/+page.svelte
+++ empi-web/src/routes/attributes/+page.svelte
@@ -3,19 +3,28 @@
 
 	import type { PageData } from './$types';
 	import Attribute from './Attribute.svelte';
+	import { goto } from '$app/navigation';
+	import { onMount } from 'svelte';
 
 	export let data: PageData;
+
+	onMount(() => {
+		// @ts-expect-error TS thinks data.user is always undefined, when it in fact isn't
+		if (!data.user?.is_staff) {
+			goto('/', { replaceState: true });
+		}
+	});
 
 	function addAttr(e: Event) {
 		const target = e.target as HTMLButtonElement;
 		const parent = target.parentElement;
-		new Attribute({target: parent!, anchor: target});
+		new Attribute({ target: parent!, anchor: target });
 	}
 </script>
 <h1>{$t('common.attributes')}</h1>
 {#each data.attrs as attr}
 	<Attribute {attr}></Attribute>
-	<button on:click={addAttr}>{$t('attrs.add')}</button>
 {:else}
 	<p>{$t('attrs.no_attrs')}</p>
 {/each}
+<button on:click={addAttr}>{$t('attrs.add')}</button>
