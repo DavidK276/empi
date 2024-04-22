1019ebb983e721ffc27e3007387f6c20a8d6aee8
David Krchňavý <david.krchnavy@gmail.com>
2024-04-22 14:42:41 +0200

    empi web: move dropdowns to component

---
+++ empi-web/src/lib/components/Dropdown.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+	import { col, dropdown, dropdownContent } from '$lib/style.css';
+
+	const toggleDropdown = (event: MouseEvent) => {
+		const target = event.target as HTMLElement;
+		const toggledDropdown = target.parentElement;
+		if (toggledDropdown != null) {
+			toggledDropdown.classList.toggle('show');
+
+			const dropdowns = document.getElementsByClassName(dropdown);
+			for (const dropdown of dropdowns) {
+				if (dropdown !== toggledDropdown) {
+					dropdown.classList.remove('show');
+				}
+			}
+		}
+	};
+
+	export let title: string;
+</script>
+
+<div class="{dropdown}">
+	<button on:click={toggleDropdown}>
+		{title}
+		<span class="material-symbols-outlined" style="pointer-events: none">expand_more</span>
+	</button>
+	<div class="{dropdownContent} {col}">
+		<slot></slot>
+	</div>
+</div>
--- empi-web/src/routes/+layout.svelte
+++ empi-web/src/routes/+layout.svelte
@@ -1,15 +1,15 @@
 <script lang="ts">
 	import '@fontsource/source-sans-pro';
 	import { enhance } from '$app/forms';
-	import { col, content, dropdown, dropdownContent, error, row } from '$lib/style.css';
+	import { content, error, row } from '$lib/style.css';
 	import { themeClass, vars } from '$lib/theme.css';
 
 	import { t } from '$lib/translations';
 	import type { LayoutServerData } from './$types';
 	import { page } from '$app/stores';
-	import { toggleDropdown } from '$lib/functions';
 	import { goto } from '$app/navigation';
 	import { store } from '$lib/stores';
+	import Dropdown from '$lib/components/Dropdown.svelte';
 
 	export let data: LayoutServerData;
 	let logging_in = false;
@@ -33,23 +33,18 @@
 			<a href="/about" style="margin: 0 {vars.sm}">{$t('common.about')}</a>
 		</nav>
 		{#if data.user != null}
-			<div class="{dropdown}">
-				<button on:click={toggleDropdown}>
-					{data.user.first_name} {data.user.last_name}
-					<span class="material-symbols-outlined">expand_more</span>
-				</button>
-				<div class="{dropdownContent}" style="flex-direction: column">
-					{#if !data.user.is_staff}
-						<div><a href="/attributes">{$t('common.attributes')}<span
-							class="material-symbols-outlined">navigate_next</span></a></div>
-					{/if}
-					<div><a href="/account">{$t('common.account')}<span class="material-symbols-outlined">navigate_next</span></a>
-					</div>
-					<div><a href="/account/points">{$t('common.my_points')}<span
-						class="material-symbols-outlined">navigate_next</span></a>
-					</div>
-					<form method="POST" action="/?/logout" on:submit={unsetPasswordSession}
-								use:enhance={() => {
+			<Dropdown title="{data.user.first_name + ' ' + data.user.last_name}">
+				{#if !data.user.is_staff}
+					<div><a href="/attributes">{$t('common.attributes')}<span
+						class="material-symbols-outlined">navigate_next</span></a></div>
+				{/if}
+				<div><a href="/account">{$t('common.account')}<span class="material-symbols-outlined">navigate_next</span></a>
+				</div>
+				<div><a href="/account/points">{$t('common.my_points')}<span
+					class="material-symbols-outlined">navigate_next</span></a>
+				</div>
+				<form method="POST" action="/?/logout" on:submit={unsetPasswordSession}
+							use:enhance={() => {
 									logging_out = true;
 
 									return async ({ update }) => {
@@ -58,23 +53,17 @@
 										logging_out = false;
 									};
 								}}>
-						{#if logging_out}
-							<button type="submit" disabled>{$t('common.logging_out')}</button>
-						{:else}
-							<button type="submit" name="submit">{$t('common.logout')}</button>
-						{/if}
-					</form>
-				</div>
-			</div>
+					{#if logging_out}
+						<button type="submit" disabled>{$t('common.logging_out')}</button>
+					{:else}
+						<button type="submit" name="submit">{$t('common.logout')}</button>
+					{/if}
+				</form>
+			</Dropdown>
 		{:else}
-			<div class="{dropdown}">
-				<button on:click={toggleDropdown}>
-					{$t('common.account')}
-					<span class="material-symbols-outlined" style="pointer-events: none">expand_more</span>
-				</button>
-				<div class="{dropdownContent}">
-					<form method="POST" action="/?/login" style="width: 100%" on:submit={setPasswordSession}
-								use:enhance={() => {
+			<Dropdown title="{$t('common.account')}">
+				<form method="POST" action="/?/login" style="width: 100%" on:submit={setPasswordSession}
+							use:enhance={() => {
 									logging_in = true;
 
 									return async ({ update }) => {
@@ -82,24 +71,23 @@
 										logging_in = false;
 									};
 								}}>
-						<label for="username">{$t('common.username')}: </label>
-						<input type="text" id="username" name="username" required>
-						<label for="password">{$t('common.password')}: </label>
-						<input type="password" id="password" name="password" required minlength="4">
-						{#if $page.form?.login === false}
-							<p class="{error}" style="white-space: nowrap">{$t('common.wrong_login')}</p>
+					<label for="username">{$t('common.username')}: </label>
+					<input type="text" id="username" name="username" required>
+					<label for="password">{$t('common.password')}: </label>
+					<input type="password" id="password" name="password" required minlength="4">
+					{#if $page.form?.login === false}
+						<p class="{error}" style="white-space: nowrap">{$t('common.wrong_login')}</p>
+					{/if}
+					<div style="display: flex; flex-wrap: nowrap">
+						{#if logging_in}
+							<button type="submit" disabled>{$t('common.logging_in')}</button>
+						{:else}
+							<button type="submit" name="submit">{$t('common.login')}</button>
 						{/if}
-						<div style="display: flex; flex-wrap: nowrap">
-							{#if logging_in}
-								<button type="submit" disabled>{$t('common.logging_in')}</button>
-							{:else}
-								<button type="submit" name="submit">{$t('common.login')}</button>
-							{/if}
-							<a href="/register">{$t('common.registration')}</a>
-						</div>
-					</form>
-				</div>
-			</div>
+						<a href="/register" style="margin: 0 {vars.sm}">{$t('common.registration')}</a>
+					</div>
+				</form>
+			</Dropdown>
 			<button>
 				<a href="/research" style="color: {vars.textSecondary}">
 					{$t('common.create_research')}
@@ -108,16 +96,10 @@
 			</button>
 		{/if}
 		{#if data.user?.is_staff}
-			<div class="{dropdown}">
-				<button on:click={toggleDropdown}>
-					{$t('common.administration')}
-					<span class="material-symbols-outlined" style="pointer-events: none">expand_more</span>
-				</button>
-				<div class="{dropdownContent} {col}" style="padding-top: {vars.lg}">
-					<div><a href="/attributes">{$t('common.attributes')}</a></div>
-					<div><a href="/admin/points">{$t('common.points')}</a></div>
-				</div>
-			</div>
+			<Dropdown title="{$t('common.administration')}">
+				<div><a href="/attributes">{$t('common.attributes')}</a></div>
+				<div><a href="/admin/points">{$t('common.points')}</a></div>
+			</Dropdown>
 		{/if}
 	</header>
 	<div class="{row} ver-top hor-center">
