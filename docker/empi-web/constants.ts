7bd94eb212caa8a7f58bb9ed6a6c707fc9bdedf7
David Krchňavý <david.krchnavy@gmail.com>
2024-05-07 15:10:32 +0200

    empi: remove old files

--- docker/empi-web/constants.ts
+++
@@ -1,9 +0,0 @@
-export const INT_SERVER_URL = 'http://api:8000/';
-export const EXT_SERVER_URL = 'http://127.0.0.1:8000/';
-export const INT_API_ENDPOINT = INT_SERVER_URL + 'api/';
-export const EXT_API_ENDPOINT = EXT_SERVER_URL + 'api/';
-export const TOKEN_COOKIE = 'sessiontoken';
-export const SELF_URL_COOKIE = 'userurl';
-export const SUPPORTED_LANGS = ['en', 'sk'];
-export const ALLOW_TEXTENTRY_ATTR = false;
-export const PAGE_SIZE = 10;
