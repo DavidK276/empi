a3ae4acf0e7943e24aaff2dc5db69d857f05b0d0
David Krchňavý <david.krchnavy@gmail.com>
2024-05-20 08:42:26 +0200

    empi web: add dark mode toggle

---
+++ empi-web/src/lib/components/ThemeToggle.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import { browser } from "$app/environment";
+
+	let theme: string | null = null;
+
+	if (browser) {
+		theme = localStorage.getItem("theme");
+	}
+
+	let themeSelect: HTMLSelectElement;
+</script>
+
+<button type="button" id="theme-toggle"
+        on:click={() => {
+						const selectedTheme = themeSelect.item((themeSelect.selectedIndex + 1) % 3)?.value || "os";
+						themeSelect.value = selectedTheme;
+						localStorage.setItem("theme", selectedTheme);
+	        }}>
+	<select id="theme-select" bind:this={themeSelect}>
+		<option value="os" selected={theme === 'os'}>OS</option>
+		<option value="light" selected={theme === 'light'}>Light</option>
+		<option value="dark" selected={theme === 'dark'}>Dark</option>
+	</select>
+</button>
--- empi-web/src/routes/+layout.svelte
+++ empi-web/src/routes/+layout.svelte
@@ -6,6 +6,7 @@
 	import Dropdown from '$lib/components/Dropdown.svelte';
 	import Login from '$lib/components/Login.svelte';
 	import Icon from "@iconify/svelte";
+	import ThemeToggle from "$lib/components/ThemeToggle.svelte";
 
 	export let data: LayoutServerData;
 </script>
@@ -18,35 +19,39 @@
 			<a href="/research">{$t('common.create_research')}</a>
 		{/if}
 	</nav>
-	{#if data.user != null}
-		<Dropdown title="{data.user.first_name + ' ' + data.user.last_name}">
-			{#if !data.user.is_staff}
-				<div><a href="/attributes">{$t('common.attributes')}
-					<Icon icon="material-symbols:keyboard-arrow-right" width="24" height="24"></Icon>
-				</a></div>
-				<div><a href="/account/points">{$t('common.my_points')}
+	<div class="row ver-center">
+		{#if data.user != null}
+			<Dropdown title="{(data.user.first_name + ' ' + data.user.last_name).trim() || $t('common.account')}">
+				{#if !data.user.is_staff}
+					<div><a href="/attributes">{$t('common.attributes')}
+						<Icon icon="material-symbols:keyboard-arrow-right" width="24" height="24"></Icon>
+					</a></div>
+					<div><a href="/account/points">{$t('common.my_points')}
+						<Icon icon="material-symbols:keyboard-arrow-right" width="24" height="24"></Icon>
+					</a>
+					</div>
+				{/if}
+				<div><a href="/account">{$t('common.account')}
 					<Icon icon="material-symbols:keyboard-arrow-right" width="24" height="24"></Icon>
 				</a>
 				</div>
-			{/if}
-			<div><a href="/account">{$t('common.account')}
-				<Icon icon="material-symbols:keyboard-arrow-right" width="24" height="24"></Icon>
-			</a>
-			</div>
-			<Login is_logged_in={true}></Login>
-		</Dropdown>
-	{:else}
-		<Dropdown title={$t('common.account')}>
-			<Login is_logged_in={false}></Login>
-		</Dropdown>
-	{/if}
-	{#if data.user?.is_staff}
-		<Dropdown title={$t('common.administration')}>
-			<div><a href="/attributes">{$t('common.attributes')}</a></div>
-			<div><a href="/admin/research-points">{$t('common.research_points')}</a></div>
-			<div><a href="/admin/student-points">{$t('common.student_points')}</a></div>
-		</Dropdown>
-	{/if}
+				<Login is_logged_in={true}></Login>
+			</Dropdown>
+		{:else}
+			<Dropdown title={$t('common.account')}>
+				<Login is_logged_in={false}></Login>
+			</Dropdown>
+		{/if}
+		{#if data.user?.is_staff}
+			<Dropdown title={$t('common.administration')}>
+				<div><a href="/attributes">{$t('common.attributes')}</a></div>
+				<div><a href="/admin/research-points">{$t('common.research_points')}</a></div>
+				<div><a href="/admin/student-points">{$t('common.student_points')}</a></div>
+			</Dropdown>
+		{/if}
+		<div style="border-left: 1px solid var(--text-primary); height: 2rem; display: inline"></div>
+		<ThemeToggle></ThemeToggle>
+	</div>
 </header>
 <div class="row ver-top hor-center">
 	<div class="content">
