cb52ac5e23fab824e433cc2dfa0a47c6015315fd
David Krchňavý <david.krchnavy@gmail.com>
2024-08-17 11:12:39 +0200

    empi web: remove old code

--- empi-web/src/routes/server/participations/research/[nanoid=nanoid]/+server.ts
+++
@@ -1,9 +0,0 @@
-import * as consts from '$lib/constants';
-
-export const POST = async ({ fetch, request, params }) => {
-	const response = await fetch(consts.INT_API_ENDPOINT + `participation/research/${params.nanoid}/get/`, {
-		body: await request.formData(),
-		method: 'POST'
-	});
-	return new Response(response.body);
-};
