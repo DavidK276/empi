8c646f59eda4ef61b9f2c32748c6f472e4a07c2f
David Krchňavý <david.krchnavy@gmail.com>
2024-08-26 18:16:32 +0200

    createinitialuser.py: set user model

--- empi-server/users/management/commands/createinitialuser.py
+++ empi-server/users/management/commands/createinitialuser.py
@@ -2,19 +2,21 @@ import os
 
 from django.contrib.auth.management.commands import createsuperuser
 
+from users.models import EmpiUser
+
 
 class Command(createsuperuser.Command):
     help = "Crate a superuser, do nothing if the user already exists"
 
     def handle(self, *args, **options):
         database = options["database"]
-        username = options[self.UserModel.USERNAME_FIELD]
+        username = options[EmpiUser.USERNAME_FIELD]
         if username is None:
-            username = os.environ.get("DJANGO_SUPERUSER_" + self.UserModel.USERNAME_FIELD.upper(), "")
+            username = os.environ.get("DJANGO_SUPERUSER_" + EmpiUser.USERNAME_FIELD.upper(), "")
         if username:
             try:
-                self.UserModel._default_manager.db_manager(database).get_by_natural_key(username)
-            except self.UserModel.DoesNotExist:
+                EmpiUser.users.db_manager(database).get_by_natural_key(username)
+            except EmpiUser.DoesNotExist:
                 super().handle(*args, **options)
                 return
             print("Initial user already exists")
