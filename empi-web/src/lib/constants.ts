d7af699650ffd743af8a26db933c76a779d501c1
David Krchňavý <david.krchnavy@gmail.com>
2024-08-28 14:22:51 +0200

    constants.ts: add trailing slash to endpoints

--- empi-web/src/lib/constants.ts
+++ empi-web/src/lib/constants.ts
@@ -5,8 +5,8 @@ if (typeof process != 'undefined') {
 	env = process.env;
 }
 
-export const INT_API_ENDPOINT = (env?.INT_API_ENDPOINT || 'http://127.0.0.1:8000/api').replace(/\/$/, '')
-export const EXT_API_ENDPOINT = (env?.EXT_API_ENDPOINT || 'http://127.0.0.1:8000/api').replace(/\/$/, '')
+export const INT_API_ENDPOINT = (env?.INT_API_ENDPOINT || 'http://127.0.0.1:8000/api').replace(/\/$/, '') + '/';
+export const EXT_API_ENDPOINT = (env?.EXT_API_ENDPOINT || 'http://127.0.0.1:8000/api').replace(/\/$/, '') + '/';
 export const TOKEN_COOKIE = 'sessiontoken';
 export const SUPPORTED_LANGS = ['en', 'sk'];
 export const ALLOW_TEXTENTRY_ATTR = false;
