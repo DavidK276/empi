d3ffb252b9bd0484189e75c08c304729dcf32a72
David Krchňavý <david.krchnavy@gmail.com>
2024-08-28 12:37:13 +0200

    constants.ts: read api prefix from env variable

--- empi-web/src/lib/constants.ts
+++ empi-web/src/lib/constants.ts
@@ -5,10 +5,11 @@ if (typeof process != 'undefined') {
 	env = process.env;
 }
 
+const API_PREFIX = (env?.API_PREFIX || 'http://127.0.0.1:8000/').replace(/\/$/, '') + '/';
 export const INT_SERVER_URL = (env?.INT_SERVER_URL || 'http://127.0.0.1:8000/').replace(/\/$/, '') + '/';
 export const EXT_SERVER_URL = (env?.EXT_SERVER_URL || 'http://127.0.0.1:8000/').replace(/\/$/, '') + '/';
-export const INT_API_ENDPOINT = INT_SERVER_URL + 'api/';
-export const EXT_API_ENDPOINT = EXT_SERVER_URL + 'api/';
+export const INT_API_ENDPOINT = INT_SERVER_URL + API_PREFIX;
+export const EXT_API_ENDPOINT = EXT_SERVER_URL + API_PREFIX;
 export const TOKEN_COOKIE = 'sessiontoken';
 export const SUPPORTED_LANGS = ['en', 'sk'];
 export const ALLOW_TEXTENTRY_ATTR = false;
