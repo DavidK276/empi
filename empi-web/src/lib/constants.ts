2a479ca1df24068574060b7bc832c959b5d88b92
David Krchňavý <david.krchnavy@gmail.com>
2024-08-28 13:00:50 +0200

    constants.ts: simplify env variables

--- empi-web/src/lib/constants.ts
+++ empi-web/src/lib/constants.ts
@@ -5,11 +5,8 @@ if (typeof process != 'undefined') {
 	env = process.env;
 }
 
-const API_PREFIX = (env?.API_PREFIX || 'http://127.0.0.1:8000/').replace(/\/$/, '') + '/';
-export const INT_SERVER_URL = (env?.INT_SERVER_URL || 'http://127.0.0.1:8000/').replace(/\/$/, '') + '/';
-export const EXT_SERVER_URL = (env?.EXT_SERVER_URL || 'http://127.0.0.1:8000/').replace(/\/$/, '') + '/';
-export const INT_API_ENDPOINT = INT_SERVER_URL + API_PREFIX;
-export const EXT_API_ENDPOINT = EXT_SERVER_URL + API_PREFIX;
+export const INT_API_ENDPOINT = (env?.INT_API_ENDPOINT || 'http://127.0.0.1:8000/api').replace(/\/$/, '')
+export const EXT_API_ENDPOINT = (env?.EXT_API_ENDPOINT || 'http://127.0.0.1:8000/api').replace(/\/$/, '')
 export const TOKEN_COOKIE = 'sessiontoken';
 export const SUPPORTED_LANGS = ['en', 'sk'];
 export const ALLOW_TEXTENTRY_ATTR = false;
