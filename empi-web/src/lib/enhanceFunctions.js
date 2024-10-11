35a67439b31aa70ce14a5ac487a50e6bc20f2067
David Krchňavý <david.krchnavy@gmail.com>
2024-10-11 16:03:47 +0200

    empi: bump version to 2024.10.11

--- empi-web/src/lib/enhanceFunctions.js
+++ empi-web/src/lib/enhanceFunctions.js
@@ -1,11 +1,13 @@
 import { addFormErrors } from "$lib/functions";
 import FormResultMessage from "$lib/components/FormResultMessage.svelte";
 
+// @ts-expect-error this is JS, no idea why TS is even screaming here
 export async function universalEnhance({formElement, submitter}, {idleMessage, runningMessage, reset, invalidateAll}) {
 	if (submitter != null) {
 		submitter.toggleAttribute('disabled');
 		submitter.innerHTML = runningMessage;
 	}
+	// @ts-expect-error this is JS, no idea why TS is even screaming here
 	return async ({update, result}) => {
 		await update({invalidateAll, reset});
 		const submitDiv = formElement.children.namedItem('submit-div');
