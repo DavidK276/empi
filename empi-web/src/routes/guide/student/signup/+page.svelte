4f6f59e1ff1ce7cd5f70453299789b862203c729
David Krchňavý <david.krchnavy@gmail.com>
2024-10-17 15:49:42 +0200

    guide: improve wording

--- empi-web/src/routes/guide/student/signup/+page.svelte
+++ empi-web/src/routes/guide/student/signup/+page.svelte
@@ -11,13 +11,13 @@
 
 <ol>
 	<li>Prihlás sa do systému svojimi prihlasovacími údajmi.</li>
-	<li>Na hlavnej stránke nájdeš zoznam výskumov, ktoré majú otvorené prihlasovanie. Vyber si jeden z nich kliknutím na tlačidlo <cite>Detaily</cite>. <a
+	<li>Na hlavnej stránke nájdeš zoznam výskumov, ktorých sa môžeš zúčastniť. Vyber si jeden z nich kliknutím na tlačidlo <cite>Detaily</cite>. <a
 		href="/">Zoberte ma tam!
 		<MaterialSymbolsArrowForward width="24px" height="24px"></MaterialSymbolsArrowForward>
 	</a>
 	</li>
-	<li>Na stránke detailov výskumu (ktorej náhľad vidíš vyššie) uvidíš okrem iného aj výpis termínov výskumu. Vyber si vhodný termín a stlač tlačidlo <cite>Prihlásiť sa</cite>.</li>
-	<li>V prípade, prihlásenia na online termín sa pri ňom zobrazí odkaz na zúčastnenie výskumu.</li>
+	<li>Na stránke detailov výskumu (ktorej náhľad vidíš vyššie) uvidíš okrem iného aj výpis termínov výskumu. Vyber si vhodný termín a prihlás sa naň stlačením tlačidla <cite>Zúčastniť sa</cite>.</li>
+	<li>V prípade prihlásenia na online termín sa pri ňom po prihlásení naň zobrazí odkaz na zúčastnenie výskumu.</li>
 </ol>
 
 <style>
