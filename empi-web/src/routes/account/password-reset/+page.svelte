54481197e94757d7f2d839796042faf14c122289
David Krchňavý <david.krchnavy@gmail.com>
2024-10-30 12:55:43 +0100

    empi web: check password match on password reset

--- empi-web/src/routes/account/password-reset/+page.svelte
+++ empi-web/src/routes/account/password-reset/+page.svelte
@@ -4,8 +4,11 @@
 	import type { ActionResult } from '@sveltejs/kit';
 	import FormResultMessage from '$lib/components/FormResultMessage.svelte';
 	import { mount } from "svelte";
+	import { addFormError, removeFormError } from "$lib/functions";
 
 	let submitButton: HTMLButtonElement;
+	let passwordInput: HTMLInputElement;
+	let repeatPasswordInput: HTMLInputElement;
 
 	async function doUpdate() {
 		return async ({ update, result }: {
@@ -24,15 +27,28 @@
 			await update();
 		};
 	}
+
+	const submit = (event: SubmitEvent) => {
+		if (passwordInput.value !== repeatPasswordInput.value) {
+			addFormError(passwordInput, $t('common.passwords_nomatch'));
+			addFormError(repeatPasswordInput, $t('common.passwords_nomatch'));
+			event.preventDefault();
+			event.stopPropagation();
+		}
+		else {
+			removeFormError(passwordInput);
+			removeFormError(repeatPasswordInput);
+		}
+	};
 </script>
 
 <h1>Resetovanie hesla</h1>
 <div class="row" style="padding-top: var(--xl);">
-	<form class="m-w-full" method="POST" style="width: 50%" use:enhance={doUpdate}>
+	<form class="m-w-full" method="POST" onsubmit={submit} style="width: 50%" use:enhance={doUpdate}>
 		<label for="new_password">Nové heslo</label>
-		<input id="new_password" name="new_password" type="password">
+		<input bind:value={passwordInput} id="new_password" name="new_password" type="password">
 		<label for="repeat_password">{$t('common.repeat_password')}</label>
-		<input id="repeat_password" type="password">
+		<input bind:value={repeatPasswordInput} id="repeat_password" type="password">
 		<div class="row ver-center">
 			<button bind:this={submitButton} type="submit">{$t('common.submit')}</button>
 		</div>
