fcb461a6b8a1d5260b7813634bb0a8fa9700a991
David Krchňavý <david.krchnavy@gmail.com>
2024-11-08 17:30:17 +0100

    empi: bump version to 2024.11.1

--- empi-server/empi_server/settings.py
+++ empi-server/empi_server/settings.py
@@ -164,7 +164,7 @@ REST_KNOX = {"USER_SERIALIZER": "users.serializers.UserSerializer", "AUTH_HEADER
 SPECTACULAR_SETTINGS = {
     "TITLE": "Empi API",
     "DESCRIPTION": "The backend API of Empi",
-    "VERSION": "2024.10.29",
+    "VERSION": "2024.11.1",
     "SERVE_INCLUDE_SCHEMA": True,
     "SERVERS": [{"url": "http://localhost:8000"}],
 }
