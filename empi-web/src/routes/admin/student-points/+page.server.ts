db16ab9724f1e17b616478a1a2e99728f1168a86
David Krchňavý <david.krchnavy@gmail.com>
2025-02-14 23:06:21 +0100

    empi web: load student points asynchronously

--- empi-web/src/routes/admin/student-points/+page.server.ts
+++ empi-web/src/routes/admin/student-points/+page.server.ts
@@ -2,6 +2,44 @@ import type { PageServerLoad } from './$types';
 import * as consts from '$lib/constants';
 import { getCurrentAcademicYear, getCurrentSemester } from "$lib/settings";
 
+async function getParticipations(fetch: Function, formData: FormData, year: string, semester: string) {
+	const response = await fetch(consts.INT_API_ENDPOINT + `participation/user/?year=${year}&semester=${semester}`, {
+		body: formData,
+		method: 'POST'
+	});
+	const responseJSON = await response.json();
+	const participationMap: Map<string, {
+		name: string,
+		unconfirmedPoints: number,
+		confirmedPoints: number
+	}> = new Map();
+	for (const participation of responseJSON) {
+		const token = participation.participant.token;
+		const name = participation.participant.user_detail.first_name + ' ' + participation.participant.user_detail.last_name;
+		const currentParticipation = participationMap.get(token) || { name, unconfirmedPoints: 0, confirmedPoints: 0 };
+		if (participation.is_confirmed) {
+			participationMap.set(token, {
+				name,
+				unconfirmedPoints: currentParticipation.unconfirmedPoints,
+				confirmedPoints: currentParticipation.confirmedPoints + participation.research.points
+			});
+		}
+		else {
+			participationMap.set(token, {
+				name,
+				unconfirmedPoints: currentParticipation.unconfirmedPoints + participation.research.points,
+				confirmedPoints: currentParticipation.confirmedPoints
+			});
+		}
+	}
+	const participations = Array.from(participationMap.values());
+	participations.sort((a, b) => {
+		const textA = a.name.toUpperCase().split(' ', 2)[1];
+		const textB = b.name.toUpperCase().split(' ', 2)[1];
+		return (textA < textB) ? -1 : (textA > textB) ? 1 : 0;
+	})
+	return participations;
+}
 
 export const load: PageServerLoad = async ({ cookies, fetch, locals, request }) => {
 	const formData = new FormData();
@@ -13,36 +51,7 @@ export const load: PageServerLoad = async ({ cookies, fetch, locals, request }) 
 	const semester = searchParams.get('semester') || getCurrentSemester(locals.session.data.settings);
 
 	if (cookies.get(consts.TOKEN_COOKIE)) {
-		const response = await fetch(consts.INT_API_ENDPOINT + `participation/user/?year=${year}&semester=${semester}`, {
-			body: formData,
-			method: 'POST'
-		});
-		const responseJSON = await response.json();
-		const participationMap: Map<string, {
-			name: string,
-			unconfirmedPoints: number,
-			confirmedPoints: number
-		}> = new Map();
-		for (const participation of responseJSON) {
-			const token = participation.participant.token;
-			const name = participation.participant.user_detail.first_name + ' ' + participation.participant.user_detail.last_name;
-			const currentParticipation = participationMap.get(token) || { name, unconfirmedPoints: 0, confirmedPoints: 0 };
-			if (participation.is_confirmed) {
-				participationMap.set(token, {
-					name,
-					unconfirmedPoints: currentParticipation.unconfirmedPoints,
-					confirmedPoints: currentParticipation.confirmedPoints + participation.research.points
-				});
-			}
-			else {
-				participationMap.set(token, {
-					name,
-					unconfirmedPoints: currentParticipation.unconfirmedPoints + participation.research.points,
-					confirmedPoints: currentParticipation.confirmedPoints
-				});
-			}
-		}
-		return { participations: Array.from(participationMap.values()) };
+		return { participations: getParticipations(fetch, formData, year, semester) };
 	}
 	return { participations: null };
 };
--- empi-web/src/routes/admin/student-points/+page.svelte
+++ empi-web/src/routes/admin/student-points/+page.svelte
@@ -18,12 +18,6 @@
 
 		goto(`?${query.toString()}`, { invalidateAll: true });
 	}
-
-	data.participations?.sort((a, b) => {
-		const textA = a.name.toUpperCase().split(' ', 2)[1];
-		const textB = b.name.toUpperCase().split(' ', 2)[1];
-		return (textA < textB) ? -1 : (textA > textB) ? 1 : 0;
-	})
 </script>
 <UserPasswordRequiredModal></UserPasswordRequiredModal>
 <h1>{$t('common.points')}</h1>
@@ -41,30 +35,34 @@
 			</select>
 		</div>
 	</div>
-	{#if data.participations && data.participations.length > 0}
-		<table style="width: 100%; max-width: 100vw">
-			<thead>
-			<tr>
-				<th>{$t('common.name')}</th>
-				<th>{$t('common.unconfirmed_points')}</th>
-				<th>{$t('common.confirmed_points')}</th>
-				<th>{$t('common.sum')}</th>
-			</tr>
-			</thead>
-			<tbody>
-			{#each data.participations as participation}
+	{#await data.participations}
+		<p>{$t('common.loading')}</p>
+	{:then participations}
+		{#if participations?.length || 0 > 0}
+			<table style="width: 100%; max-width: 100vw">
+				<thead>
 				<tr>
-					<td>{participation.name}</td>
-					<td style="text-align: center">{participation.unconfirmedPoints}</td>
-					<td style="text-align: center">{participation.confirmedPoints}</td>
-					<td style="text-align: center">{participation.unconfirmedPoints + participation.confirmedPoints}</td>
+					<th>{$t('common.name')}</th>
+					<th>{$t('common.unconfirmed_points')}</th>
+					<th>{$t('common.confirmed_points')}</th>
+					<th>{$t('common.sum')}</th>
 				</tr>
-			{/each}
-			</tbody>
-		</table>
-	{:else}
-		<p>{$t('common.no_students')}</p>
-	{/if}
+				</thead>
+				<tbody>
+				{#each participations || [] as participation}
+					<tr>
+						<td>{participation.name}</td>
+						<td style="text-align: center">{participation.unconfirmedPoints}</td>
+						<td style="text-align: center">{participation.confirmedPoints}</td>
+						<td style="text-align: center">{participation.unconfirmedPoints + participation.confirmedPoints}</td>
+					</tr>
+				{/each}
+				</tbody>
+			</table>
+		{:else}
+			<p>{$t('common.no_students')}</p>
+		{/if}
+	{/await}
 </div>
 
 <style>
