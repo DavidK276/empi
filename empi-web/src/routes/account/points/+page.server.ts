a2ea3ee67a4e9682f79787b0136d83dcc94fd4b9
David Krchňavý <david.krchnavy@gmail.com>
2025-02-15 21:55:39 +0100

    fix viewing participations as admin

--- empi-web/src/routes/account/points/+page.server.ts
+++ empi-web/src/routes/account/points/+page.server.ts
@@ -19,18 +19,17 @@ async function loadParticipations(fetch: Function, formData: FormData, year: str
 	return participations;
 }
 
-export const load: PageServerLoad = async ({ cookies, fetch, parent, request }) => {
-	const { session } = await parent();
+export const load: PageServerLoad = async ({ cookies, fetch, locals, request }) => {
 
 	const response = await fetch(INT_API_ENDPOINT + 'participation/academic_year_choices/');
 	const academic_year_choices = await response.json();
 
 	const formData = new FormData();
-	formData.set('password', session?.user_password);
+	formData.set('password', locals.session.data.user_password);
 	if (cookies.get(consts.TOKEN_COOKIE)) {
 		const searchParams = new URL(request.url).searchParams;
-		const year = searchParams.get('year') || getCurrentAcademicYear(session.data?.settings);
-		const semester = searchParams.get('semester') || getCurrentSemester(session.data?.settings);
+		const year = searchParams.get('year') || getCurrentAcademicYear(locals.session.data.settings);
+		const semester = searchParams.get('semester') || getCurrentSemester(locals.session.data.settings);
 
 
 		return { participations: loadParticipations(fetch, formData, year, semester), academic_year_choices }
