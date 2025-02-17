d5d88d857fa12e7ee5ab2a67e75052c0a7705667
David Krchňavý <david.krchnavy@gmail.com>
2025-02-17 14:09:12 +0100

    empi web: fix Modal positioning in the viewport

--- empi-web/src/lib/components/Modal.svelte
+++ empi-web/src/lib/components/Modal.svelte
@@ -40,39 +40,45 @@
 </dialog>
 
 <style>
-    dialog {
-        max-width: 32em;
-        border-radius: 0.2em;
-        border: none;
-    }
+	dialog {
+		border-radius: var(--xs);
+		border: none;
 
-    dialog::backdrop {
-        background: rgba(0, 0, 0, 0.3);
-    }
+		position: fixed;
+		inset: 0;
+		width: 24rem;
+		max-width: 100vw;
+		max-height: 100dvh;
+		margin: auto;
+	}
 
-    dialog[open] {
-        animation: zoom 0.3s cubic-bezier(0.34, 1.56, 0.64, 1);
-    }
+	dialog::backdrop {
+		background: rgba(0, 0, 0, 0.3);
+	}
 
-    @keyframes zoom {
-        from {
-            transform: scale(0.95);
-        }
-        to {
-            transform: scale(1);
-        }
-    }
+	dialog[open] {
+		animation: zoom 0.3s cubic-bezier(0.34, 1.56, 0.64, 1);
+	}
 
-    dialog[open]::backdrop {
-        animation: fade 0.2s ease-out;
-    }
+	@keyframes zoom {
+		from {
+			transform: scale(0.95);
+		}
+		to {
+			transform: scale(1);
+		}
+	}
 
-    @keyframes fade {
-        from {
-            opacity: 0;
-        }
-        to {
-            opacity: 1;
-        }
-    }
+	dialog[open]::backdrop {
+		animation: fade 0.2s ease-out;
+	}
+
+	@keyframes fade {
+		from {
+			opacity: 0;
+		}
+		to {
+			opacity: 1;
+		}
+	}
 </style>
