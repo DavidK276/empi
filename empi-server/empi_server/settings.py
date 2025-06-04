3dd6c266462fb0149b3b12c7f69a3292b85d389b
David Krchňavý <david.krchnavy@gmail.com>
2025-06-04 11:23:46 +0200

    empi: version 2025.6.1

--- empi-server/empi_server/settings.py
+++ empi-server/empi_server/settings.py
@@ -165,7 +165,7 @@ REST_KNOX = {"USER_SERIALIZER": "users.serializers.UserSerializer", "AUTH_HEADER
 SPECTACULAR_SETTINGS = {
     "TITLE": "Empi API",
     "DESCRIPTION": "The backend API of Empi",
-    "VERSION": "2025.2.2",
+    "VERSION": "2025.6.1",
     "SERVE_INCLUDE_SCHEMA": True,
     "SERVERS": [{"url": "http://localhost:8000"}],
 }
