100ea6498997bbbd19ad0786b805ffcd3dbefd1d
David Krchňavý <krchnavy11@uniba.sk>
2025-10-06 16:30:15 +0200

    empi web: add headings to main page

--- empi-web/src/routes/+page.svelte
+++ empi-web/src/routes/+page.svelte
@@ -1,51 +1,53 @@
 <script lang="ts">
-	import type { PageData } from './$types';
-	import { t } from '$lib/translations';
-	import Pagination from '$lib/components/Pagination.svelte';
-	import { onMount } from "svelte";
+    import type { PageData } from './$types';
+    import { t } from '$lib/translations';
+    import Pagination from '$lib/components/Pagination.svelte';
+    import { onMount } from "svelte";
 
-	let { data }: { data: PageData } = $props();
-	let showInfoUrlColumn = $state(false);
+    let { data }: { data: PageData } = $props();
+    let showInfoUrlColumn = $state(false);
 
-	onMount(() => {
-		for (const research of data.researches) {
-			showInfoUrlColumn ||= research.info_url != null;
-		}
-	});
+    onMount(() => {
+        for (const research of data.researches) {
+            showInfoUrlColumn ||= research.info_url != null;
+        }
+    });
 </script>
 
-<h1>EMPI</h1>
+<h1>EMPI <span style="font-weight: normal">- Účasť na Empirickom Výskume (2-MXX-132)</span></h1>
 <div style="overflow-x: auto">
-	<table style="width: 100%; max-width: 100vw">
-		<thead>
-		<tr>
-			<th>{$t('research.name')}</th>
-			{#if showInfoUrlColumn}
-				<th>{$t('common.more_info')}</th>
-			{/if}
-			<th>{$t('common.details')}</th>
-		</tr>
-		</thead>
-		<tbody>
-		{#each data.researches as research}
-			{#if research.has_open_appointments}
-				<tr>
-					<td>{research.name}</td>
-					{#if showInfoUrlColumn}
-						<td style="text-align: center"><a href="{research.info_url}" target="_blank">{$t('common.learn_more')}</a>
-						</td>
-					{/if}
-					<td style="text-align: center">
-						<button>
-							<a href="research/{research.id}/">{$t('common.details')}</a>
-						</button>
-					</td>
-				</tr>
-			{/if}
-		{/each}
-		</tbody>
-	</table>
+    <h2>Aktuálne zverejnené výskumy</h2>
+    <table style="width: 100%; max-width: 100vw">
+        <thead>
+        <tr>
+            <th>{$t('research.name')}</th>
+            {#if showInfoUrlColumn}
+                <th>{$t('common.more_info')}</th>
+            {/if}
+            <th>{$t('common.details')}</th>
+        </tr>
+        </thead>
+        <tbody>
+        {#each data.researches as research}
+            {#if research.has_open_appointments}
+                <tr>
+                    <td>{research.name}</td>
+                    {#if showInfoUrlColumn}
+                        <td style="text-align: center"><a href="{research.info_url}"
+                                                          target="_blank">{$t('common.learn_more')}</a>
+                        </td>
+                    {/if}
+                    <td style="text-align: center">
+                        <button>
+                            <a href="research/{research.id}/">{$t('common.details')}</a>
+                        </button>
+                    </td>
+                </tr>
+            {/if}
+        {/each}
+        </tbody>
+    </table>
 </div>
 <div class="row" style="margin-top: var(--sm)">
-	<Pagination count={data.count}></Pagination>
+    <Pagination count={data.count}></Pagination>
 </div>
